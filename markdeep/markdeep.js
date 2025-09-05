4f846cc4dd4540d18652aadd6c3b3e6b21cafd73
ChemStud24 <will.johnson@yale.edu>
2025-09-05 12:00:42 -0400

    hyperlink color contrast for accessibility

--- markdeep/markdeep.js
+++ markdeep/markdeep.js
@@ -362,7 +362,7 @@ var STYLESHEET = entag('style',
     '}' +
 
     '.md a.target{width:0px;height:0px;visibility:hidden;font-size:0px;display:inline-block}' +
-    '.md a:link, .md a:visited{color:#38A;text-decoration:none}' +
+    '.md a:link, .md a:visited{color:#005999;text-decoration:none}' +
     '.md a:link:hover{text-decoration:underline}' +
 
     '.md dt{' +
